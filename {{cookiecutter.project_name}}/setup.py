44b496df24fcb76499a51233cb48cdcefce9c4df
mahora65 <jitrayu.p@gmail.com>
2023-03-04 13:37:53 +0100

    feat: add setup.py and config requirements

---
+++ {{cookiecutter.project_name}}/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name= "src",
+    packages= find_packages(),
+    version= '0.1.0',
+    description= '{{cookiecutter.project_short_description}}',
+    author= '{{cookiecutter.full_name}}',
+    author_email= '{{cookiecutter.email}}'
+)
